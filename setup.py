2c6b4ec22fe360d9c8ec55bf18f0627349c87fc1
Martin Pool <mbp@sourcefrog.net>
2013-06-16 17:51:32 +1000

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='shellliketests',
+      version='0.1.0',
+      description='Write command-line tests using transcript examples',
+      maintainer='Martin Pool',
+      maintainer_email='mbp@sourcefrog.net',
+      packages=['shellliketests'],
+      license='License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+     )
